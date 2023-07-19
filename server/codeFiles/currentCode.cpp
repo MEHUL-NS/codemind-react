c055667c1bba187a38e7872380d086cdf7078e4a
Mehul Sutrave <sutravenmehul@gmail.com>
2023-07-19 12:36:23 +0530

    C also added

--- server/codeFiles/currentCode.cpp
+++ server/codeFiles/currentCode.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello";
-}i++){
-        for(int j=1;j<=i;j++){
-            cout<<j;
-        }
+   for(int i=5;i>0;i--){
+    for(int j=1;j<=i;j++)
+        cout<<j;
+
         cout<<endl;
-    }
+   }
+
+    
 }
--- server/index.js
+++ server/index.js
@@ -5,73 +5,143 @@ import cookieParser from 'cookie-parser';
 import router from './Routes/routes.js'
 import fs from 'fs'
 import path from 'path';
-import {exec} from 'child_process'
+import { exec } from 'child_process'
 let port = 5000
 
 const app = express();
 app.use(cors())
 app.use(express.static('public'));
-app.use(bodyParser.json({extended:true})) 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/' , router)
+app.use('/', router)
+
+app.post("/compile", (req, res) => {
+    const code = req.body.code;
+    const lang = req.body.language
+    console.log("code : ", code)
+    console.log("lang : ", lang)
+
+    if (lang === 'cpp') {
+        let fileName = 'currentCode.cpp'
+        let filePath = `./codeFiles/${fileName}`
+
+        fs.writeFile(filePath, code, (err) => {
+            if (err) {
+                console.error('Error saving file:', err);
+                return;
+            }
+
+            console.log('File saved successfully.');
+
+            const compileCommand = `g++ -o compiledCode.exe ${filePath}`;
 
 
-app.post("/compile" , (req,res) => {
-    const code = req.body.code;   
-    const lang = req.body.lang
+            exec(compileCommand, (error, stdout, stderr) => {
+                if (error) {
+                    console.error('Compilation error:', error);
+                    return;
+                }
 
-    let fileName = 'currentCode.cpp'
-    let filePath = `./codeFiles/${fileName}`
-   
-    fs.writeFile(filePath, code, (err) => {
-      if (err) {
-        console.error('Error saving file:', err);       
-        return;
-      }
+                console.log('Compilation successful.');
 
-      console.log('File saved successfully.');
-      
-      const compileCommand = `g++ -o compiledCode.exe ${filePath}`;
+                const executionCommand = path.join('./', 'compiledCode.exe');
 
-    
-      exec(compileCommand, (error, stdout, stderr) => {
-        if (error) {
-          console.error('Compilation error:', error);
-          
-      return;
+
+                exec(executionCommand, (error, stdout, stderr) => {
+                    if (error) {
+                        console.error('Execution error:', error);
+
+                        return;
+                    }
+
+                    console.log('Execution successful.');
+                    console.log('Program output:', stdout);
+                    console.log('Program error:', stderr);
+
+                });
+            });
+        })
+    }
+    else if (lang === 'python') {
+        const fileName = 'code.py';
+        const filePath = `./codeFiles/${fileName}`;
+
+        fs.writeFile(filePath, code, (err) => {
+            if (err) {
+                console.error('Error saving file:', err);
+                return;
+            }
+
+            console.log('File saved successfully.');
+
+
+            const executionCommand = `python ${filePath}`;
+
+            exec(executionCommand, (error, stdout, stderr) => {
+                if (error) {
+                    console.error('Execution error:', error);
+                    return;
+                }
+                console.log('Execution successful.');
+                console.log('Program output:', stdout);
+                console.log('Program error:', stderr);
+
+            })
+        })
+    }
+    else if (lang === 'c') {
+        const fileName = 'code.c';
+        const filePath = `./codeFiles/${fileName}`;
+
+
+        fs.writeFile(filePath, code, (err) => {
+            if (err) {
+                console.error('Error saving file:', err);
+                return;
+            }
+
+            console.log('File saved successfully.');
+
+
+            const compileCommand = `gcc -o compiledcode ${filePath}`;
+
+            exec(compileCommand, (error, stdout, stderr) => {
+                if (error) {
+                    console.error('Compilation error:', error);
+                    return;
+                }
+
+                console.log('Compilation successful.');
+
+                const executionCommand = path.join('./', 'compiledcode.exe');
+
+                exec(executionCommand, (error, stdout, stderr) => {
+                    if (error) {
+                        console.error('Execution error:', error);
+                        return;
+                    }
+
+                    console.log('Execution successful.');
+                    console.log('Program output:', stdout);
+                    console.log('Program error:', stderr);
+
+                });
+            });
+        });
+
     }
 
-    console.log('Compilation successful.');
-           
-    // let executionCommand = './compiledCode.exe'
-    const executionCommand = path.join('./', 'compiledCode.exe');
 
-
-    exec(executionCommand, (error, stdout, stderr) => {
-      if (error) {
-        console.error('Execution error:', error);
-      
-        return;
-      }
-
-      console.log('Execution successful.');
-      console.log('Program output:', stdout);
-      console.log('Program error:', stderr);
-      
-      
-    });
-  });
-})
 })
 
-app.get("/health", (req,res)=>{
+app.get("/health", (req, res) => {
     res.json("Server is running");
 })
 
-app.listen(port , ()=> {
+app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
